e55572546d8db58e7d19b148046fe11b41d51b33
Soumik Sen <mastersoumik2003@gmail.com>
2024-06-28 14:09:06 +0530

    Adding initial backend setup to repo

---
+++ Backend/src/utils/asyncHandler.js
@@ -0,0 +1,8 @@
+//Using promises
+const asyncHandler = (requestHandler) => {
+  return (req, res, next) => {
+    Promise.resolve(requestHandler()).catch((err) => next(err));
+  };
+};
+
+export { asyncHandler };
